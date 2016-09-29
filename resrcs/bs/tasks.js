9443efead9f1f99cb86f01aad74abcfc1d5be2df
Cortney <cortneyknorr@supinfo.com>
2016-09-29 13:52:36 +0200

    alias ba for build-all

--- resrcs/bs/tasks.js
+++ resrcs/bs/tasks.js
@@ -13,4 +13,5 @@ module.exports = {
     'build-static': src.static.build,
     'build-front': ['arch-server', 'build-js', 'build-static', 'build-css', 'build-views', 'build-ts'],
     'build-all': ['build-front', 'build-server'],
+    'ba': ['build-front', 'build-server'],
 };
