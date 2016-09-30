c36b8e2c9da53abed9a8fd8e36a1b0c954f3bfee
Cortney <cortneyknorr@supinfo.com>
2016-09-30 15:07:25 +0200

    bs fixes

--- resrcs/bs/io.js
+++ resrcs/bs/io.js
@@ -8,6 +8,6 @@ module.exports = {
     static: {in: 'sources/client/public/**/*', out: 'server/public/'},
     stylesheets: {in: 'sources/client/assets/stylesheets/**/*.main.scss', out: 'server/assets/stylesheets/'},
     views: {in: "sources/client/views/**/*", out: "server/views/"},
-    javascript: {in: "sources/client/assets/javascript", out: "server/assets/javascript"},
-    typescript: {in: "sources/client/assets/javascript", out: "server/assets/javascript/compiled"}
+    javascript: {in: "sources/client/assets/javascript/**/**.js", out: "server/assets/javascript"},
+    typescript: {in: "sources/client/assets/typescript/**/**.ts", out: "server/assets/javascript/compiled"}
 };
