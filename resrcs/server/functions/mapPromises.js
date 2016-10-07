7158b14a0cdb9516735b953a7201dabae5b1cc9c
Cortney <cortneyknorr@supinfo.com>
2016-10-07 15:03:36 +0200

    mapPromises

--- resrcs/server/functions/mapPromises.js
+++ resrcs/server/functions/mapPromises.js
@@ -3,7 +3,7 @@ function mapPromises(promiseObj) {
     let promiseArray = [];
     let indexReferer = [];
     for (let index in promiseObj) {
-        if (typeof promiseObj[index] == "object" && typeof promiseObj[index] == "function") {
+        if (typeof promiseObj[index] == "object" && typeof promiseObj[index].then == "function") {
             indexReferer.push(index);
             promiseArray.push(promiseObj[index]);
         } else {
@@ -12,9 +12,10 @@ function mapPromises(promiseObj) {
     }
     return Promise.all(promiseArray)
         .then(data => {
-            for (let index = 0; i < data.length; index++) {
+            for (let index = 0; index < data.length; index++) {
                 obj[indexReferer[index]] = data[index];
             }
+            return obj;
         })
         ;
 }
