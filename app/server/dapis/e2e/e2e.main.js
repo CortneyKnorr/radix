b43fb5f63e1dc1af82c0bd4707c7f14bc17636b1
cortney <ressakan@gmail.com>
2016-11-14 15:45:02 +0100

    Fixes and tests

--- app/server/dapis/e2e/e2e.main.js
+++ app/server/dapis/e2e/e2e.main.js
@@ -44,20 +44,20 @@ function stack_dapis_e2e() {
                     try {
                         let elem = yield driver.findElement(By.css(prefix + " [name=\"" + key + "\"]"));
                         let tag = yield elem.getTagName();
-                        if(tag == "input"){
+                        if (tag == "input") {
                             let type = yield elem.getAttribute("type");
                             // console.log(type);
-                            switch (type){
+                            switch (type) {
                                 case "checkbox":
-                                    if(obj[key]){
+                                    if (obj[key]) {
                                         yield elem.click();
                                     }
                                     break;
                                 case "radio":
                                     let radios = yield driver.findElements(By.css(prefix + " [name=\"" + key + "\"]"));
-                                    for(let radio of radios){
+                                    for (let radio of radios) {
                                         let value = yield radio.getAttribute("value");
-                                        if(value == obj[key]){
+                                        if (value == obj[key]) {
                                             radio.click();
                                         }
                                     }
@@ -66,13 +66,13 @@ function stack_dapis_e2e() {
                                     yield elem.sendKeys(obj[key]);
                                     break;
                             }
-                        } else if(tag == "select") {
+                        } else if (tag == "select") {
                             yield elem.click();
                             let children = yield elem.findElements(By.tagName("option"));
                             let chosenChild;
-                            for(let child of children){
+                            for (let child of children) {
                                 let text = yield child.getText();
-                                if(text == obj[key]){
+                                if (text == obj[key]) {
                                     child.click();
                                     break;
                                 }
@@ -98,7 +98,7 @@ function stack_dapis_e2e() {
                 console.log("Form tested");
                 return submitted;
             },
-            testForm: function*(formId, obj, submitSelector,delayBeforeSubmit) {
+            testForm: function*(formId, obj, submitSelector, delayBeforeSubmit) {
                 return Array.isArray(obj) ?
                     yield* thisDapi.cfs.testFormMulti(formId, obj, submitSelector, delayBeforeSubmit) :
                     yield* thisDapi.cfs.testFormOnce(formId, obj, submitSelector, delayBeforeSubmit)
@@ -132,7 +132,7 @@ function stack_dapis_e2e() {
                     console.log(`Trying to click on (${selector})`);
                     let driver = thisDapi.driver;
                     let success = typeof (yield driver.findElement(By.css(selector)).click().catch(eHandler)) == "object";
-                    if(!success){
+                    if (!success) {
                         eHandler("Apparently operation failed");
                     } else {
                         console.log("Success")
@@ -154,24 +154,23 @@ function stack_dapis_e2e() {
                 });
             },
             waitUntil: {
-                _gen: function*(func, selector, delay){
+                _gen: function*(func, selector, delay) {
                     let driver = thisDapi.driver;
                     let elem = yield driver.findElement(By.css(selector));
                     let result = yield driver.wait(until[func](elem), delay).catch(eHandler);
                     return typeof result == "object";
                 },
-                _genWithParam: function*(func, selector, param, delay){
+                _genWithParam: function*(func, selector, param, delay) {
                     let driver = thisDapi.driver;
-                    until.element
                     let elem = yield driver.findElement(By.css(selector));
                     let result = yield driver.wait(until[func](elem, param), delay).catch(eHandler);
                     return typeof result == "object";
                 },
                 elementIs: {
-                    enabled: function*(selector, delay){
+                    enabled: function*(selector, delay) {
                         console.log();
                         console.log(Boolean(delay) ?
-                            `Waiting for (${selector}) to be enabled`:
+                            `Waiting for (${selector}) to be enabled` :
                             `Waiting for (${selector}) to be enabled but will continue after ${delay}`
                         );
                         let result = yield* thisDapi.cfs.waitUntil._gen("elementIsEnabled", selector, delay);
@@ -179,10 +178,10 @@ function stack_dapis_e2e() {
                         console.log(`Success: ${result}`);
                         return result;
                     },
-                    disabled: function*(selector, delay){
+                    disabled: function*(selector, delay) {
                         console.log();
                         console.log(Boolean(delay) ?
-                            `Waiting for (${selector}) to be disabled`:
+                            `Waiting for (${selector}) to be disabled` :
                             `Waiting for (${selector}) to be disabled but will continue after ${delay}`
                         );
                         let result = yield* thisDapi.cfs.waitUntil._gen("elementIsDisabled", selector, delay);
@@ -190,10 +189,10 @@ function stack_dapis_e2e() {
                         console.log(`Success: ${result}`);
                         return result;
                     },
-                    visible: function*(selector, delay){
+                    visible: function*(selector, delay) {
                         console.log();
                         console.log(Boolean(delay) ?
-                            `Waiting for (${selector}) to be visible but will continue after ${delay}`:
+                            `Waiting for (${selector}) to be visible but will continue after ${delay}` :
                             `Waiting for (${selector}) to be visible`
                         );
                         let result = yield* thisDapi.cfs.waitUntil._gen("elementIsVisible", selector, delay);
@@ -201,10 +200,10 @@ function stack_dapis_e2e() {
                         console.log(`Success: ${result}`);
                         return result;
                     },
-                    notVisible: function*(selector, delay){
+                    notVisible: function*(selector, delay) {
                         console.log();
                         console.log(Boolean(delay) ?
-                            `Waiting for (${selector}) to not be visible`:
+                            `Waiting for (${selector}) to not be visible` :
                             `Waiting for (${selector}) to not be visible but will continue after ${delay}`
                         );
                         let result = yield* thisDapi.cfs.waitUntil._gen("elementIsVisible", selector, delay);
@@ -212,10 +211,10 @@ function stack_dapis_e2e() {
                         console.log(`Success: ${result}`);
                         return result;
                     },
-                    selected: function*(selector, delay){
+                    selected: function*(selector, delay) {
                         console.log();
                         console.log(Boolean(delay) ?
-                            `Waiting for (${selector}) to be selected`:
+                            `Waiting for (${selector}) to be selected` :
                             `Waiting for (${selector}) to be selected but will continue after ${delay}`
                         );
                         let result = yield* thisDapi.cfs.waitUntil._gen("elementIsVisible", selector, delay);
@@ -223,10 +222,10 @@ function stack_dapis_e2e() {
                         console.log(`Success: ${result}`);
                         return result;
                     },
-                    notSelected: function*(selector, delay){
+                    notSelected: function*(selector, delay) {
                         console.log();
                         console.log(Boolean(delay) ?
-                            `Waiting for (${selector}) to not be selected`:
+                            `Waiting for (${selector}) to not be selected` :
                             `Waiting for (${selector}) to not be selected but will continue after ${delay}`
                         );
                         let result = yield* thisDapi.cfs.waitUntil._gen("elementIsNotSelected", selector, delay);
@@ -236,21 +235,21 @@ function stack_dapis_e2e() {
                     },
                 },
                 elementText: {
-                    contains: function*(selector, text, delay){
+                    contains: function*(selector, text, delay) {
                         console.log();
                         console.log(Boolean(delay) ?
-                            `Waiting for (${selector})'s text to contain |${text}|.`:
-                            `Waiting for (${selector})'s text to contain |${text}| but will continue after ${delay}`
+                            `Waiting for (${selector})'s text to contain |${text}| but will continue after ${delay}` :
+                            `Waiting for (${selector})'s text to contain |${text}|.`
                         );
                         let result = yield* thisDapi.cfs.waitUntil._genWithParam("elementTextContains", selector, text, delay);
                         result = Boolean(result);
                         console.log(`Success: ${result}`);
                         return result;
                     },
-                    equals: function*(selector, text, delay){
+                    equals: function*(selector, text, delay) {
                         console.log();
                         console.log(Boolean(delay) ?
-                            `Waiting for (${selector})'s text to equals |${text}|.`:
+                            `Waiting for (${selector})'s text to equals |${text}|.` :
                             `Waiting for (${selector})'s text to equals |${text}| but will continue after ${delay}`
                         );
                         let result = yield* thisDapi.cfs.waitUntil._genWithParam("elementTextIs", selector, text, delay);
@@ -258,10 +257,10 @@ function stack_dapis_e2e() {
                         console.log(`Success: ${result}`);
                         return result;
                     },
-                    matches: function*(selector, regex, delay){
+                    matches: function*(selector, regex, delay) {
                         console.log();
                         console.log(Boolean(delay) ?
-                            `Waiting for (${selector})'s text to match |${regex}|`:
+                            `Waiting for (${selector})'s text to match |${regex}|` :
                             `Waiting for (${selector})'s text to match |${regex}| but will continue after ${delay}`
                         );
                         let result = yield* thisDapi.cfs.waitUntil._genWithParam("elementTextMatches", selector, regex, delay);
@@ -274,15 +273,15 @@ function stack_dapis_e2e() {
                     matches: function*(selector, regex, delay) {
                         console.log();
                         console.log(Boolean(delay) ?
-                            `Waiting for (${selector})'s value to match |${regex}|`:
-                            `Waiting for (${selector})'s value to match |${regex}| but will continue after ${delay}`
+                            `Waiting for (${selector})'s value to match |${regex}| but will continue after ${delay}ms` :
+                            `Waiting for (${selector})'s value to match |${regex}|`
                         );
                         let driver = thisDapi.driver;
                         let elem = yield driver.findElement(By.css(selector));
                         let cond = function () {
                             return elem.getAttribute("value")
                                 .then(value => {
-                                    if(value){
+                                    if (value) {
                                         return value.match(regex);
                                     } else {
                                         return false;
@@ -290,7 +289,7 @@ function stack_dapis_e2e() {
                                 })
                         };
                         let result = delay ?
-                            (yield driver.wait(cond, delay)):
+                            (yield driver.wait(cond, delay)) :
                             (yield driver.wait(cond));
                         result = Boolean(result);
                         console.log(`Success: ${result}`);
@@ -299,7 +298,7 @@ function stack_dapis_e2e() {
                     equals: function*(selector, text, delay) {
                         console.log();
                         console.log(Boolean(delay) ?
-                            `Waiting for (${selector})'s value to equal |${text}|`:
+                            `Waiting for (${selector})'s value to equal |${text}|` :
                             `Waiting for (${selector})'s value to equal |${text}| but will continue after ${delay}`
                         );
                         let driver = thisDapi.driver;
@@ -314,9 +313,56 @@ function stack_dapis_e2e() {
                         console.log(`Success: ${result}`);
                         return result;
                     },
+                },
+                title: {
+                    contains: function*(text, delay) {
+                        let driver = thisDapi.driver;
+                        console.log();
+                        console.log(delay ?
+                            `Waiting for title's value to contain |${text}| but will continue after ${delay}ms` :
+                            `Waiting for title's value to contain |${text}|`
+                        );
+                        console.log(yield driver.getTitle());
+                        let result = delay ?
+                            (yield driver.wait(until.titleContains(text), delay).catch(eHandler)) :
+                            (yield driver.wait(until.titleContains(text)).catch(eHandler));
+                        result = Boolean(result);
+                        console.log(`Success: ${result}`);
+                        return result;
+                    },
+                    matches: function*(text, delay) {
+                        let driver = thisDapi.driver;
+                        console.log();
+                        console.log(Boolean(delay) ?
+                            `Waiting for title's value to matches |${text}|` :
+                            `Waiting for title's value to matches |${text}| but will continue after ${delay}`
+                        );
+
+                        let result = delay ?
+                            (yield driver.wait(until.titleMatches(text), delay).catch(eHandler)) :
+                            (yield driver.wait(until.titleMatches(text)).catch(eHandler));
+                        result = Boolean(result);
+                        console.log(`Success: ${result}`);
+                        return result;
+                    },
+                    equals: function*(text, delay) {
+                        let driver = thisDapi.driver;
+                        console.log();
+                        console.log(Boolean(delay) ?
+                            `Waiting for title's value to equals |${text}|` :
+                            `Waiting for title's value to equals |${text}| but will continue after ${delay}`
+                        );
+
+                        let result = delay ?
+                            (yield driver.wait(until.titleIs(text), delay).catch(eHandler)) :
+                            (yield driver.wait(until.titleIs(text)).catch(eHandler));
+                        result = Boolean(result);
+                        console.log(`Success: ${result}`);
+                        return result;
+                    },
                 }
             },
-            close: function*(){
+            close: function*() {
                 return yield thisDapi.driver.quit();
             }
         }
--- sources/hooks/tests.js
+++ sources/hooks/tests.js
@@ -2,19 +2,24 @@ function* hooks_tests() {
     const e2e = stack.dapis.e2e;
     const action = e2e.cfs;
 
-    let homePageTests = new E2eFeatureBatch([
+    let homePageBatch = new E2eFeatureBatch([
         {
             name: "Sign up",
             url: "http://www.facebook.com/",
             delay: 20,
             tests: [
-                new E2eTest("Click on inexistant", action.clickOn.element("#u_0_3asda")).invert(true).tag("example"),
-                new E2eTest("Click first name", action.clickOn.element("#u_0_1")),
-                new E2eTest("Enter first name", action.sendKeys("Cortney")),
-                new E2eTest("Click last name", action.clickOn.element("#u_0_3")).linked(true).dependsOf("example"),
-                new E2eTest("Enter last name", action.sendKeys("Jordan")),
-                new E2eTest("Click mail", action.clickOn.element("#u_0_5")),
-                new E2eTest("Enter email", action.sendKeys("cortneyknorr@gmail.com")),
+                new E2eTest("Sign up form", action.testForm("reg",  {
+                    "firstname": "Cortney",
+                    "lastname": "Knorr",
+                     "reg_email__": "cortneyknorr@gmail.com",
+                    "reg_email_confirmation__": "cortneyknorr@gmail.com",
+                    "reg_passwd__": "fang1246",
+                    "birthday_month": "Jan",
+                    "birthday_day": "17",
+                    "birthday_year": "1995",
+                    "sex": "2",
+                }, "#u_0_e")),
+                new E2eTest("Check url", action.waitUntil.title.contains("Forgot Password", 5000)).tag("Url check"),
             ]
         },
         {
@@ -25,10 +30,11 @@ function* hooks_tests() {
                 new E2eTest("Enter login", action.sendKeys("cortneyknorr@gmail.com")),
                 new E2eTest("Click password", action.clickOn.element("#pass")),
                 new E2eTest("Enter password", action.sendKeys("fang1246")),
-                new E2eTest("Click log in", action.clickOn.element("#u_0_p")),
+                new E2eTest("Click log in", action.clickOn.element("#u_0_n")),
             ]
         },
     ]);
 
-    yield homePageTests.testFeature("Sign up");
+    yield homePageBatch.testFeature("Sign up");
+    // yield homePageBatch.testAllFeatures();
 }
