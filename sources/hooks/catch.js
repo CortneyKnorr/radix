d17160a404a81a34029e989954ae90ace731a378
Cortney Knorr <cortneyknorr@gmail.com>
2017-02-20 10:28:33 +0100

    Cleanup

--- sources/hooks/catch.js
+++ sources/hooks/catch.js
@@ -1,9 +1,5 @@
 function* hooks_catch() {
     return {
-        default: radix.dapis.useful.ehgs.plug(r => r.errors.toString()),
-        404: function*(request, response, next){
-            request.session.number = request.session.number ? request.session.number + 1: 1;
-            response.send(request.session);
-        }
+        default: radix.dapis.useful.ehgs.plug(r => r.errors.toString())
     }
 }
